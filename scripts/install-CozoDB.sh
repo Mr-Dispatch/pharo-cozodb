87de83d64d0be5d63d9685e58ab7730eb41e5a60
Astares <astares@gmx.de>
2023-09-14 21:43:07 +0200

    Add steps to build to check failing

--- scripts/install-CozoDB.sh
+++ scripts/install-CozoDB.sh
@@ -2,7 +2,23 @@
 
 # If you want to update to a newer version check https://github.com/cozodb/cozo/tags
 
-# download, extract and rename into "libcozo_c.so"
+current_directory=$(pwd)
+echo "Current directory: $current_directory"
+
+# Download the gzipped library file from the Cozo GitHub releases
+echo "Downloading library archive from https://github.com/cozodb/cozo"
 sudo wget https://github.com/cozodb/cozo/releases/download/v0.7.2/libcozo_c-0.7.2-x86_64-unknown-linux-gnu.so.gz 
-sudo gzip -d libcozo_c-0.7.2-x86_64-unknown-linux-gnu.so.gz
-sudo mv libcozo_c-0.7.2-x86_64-unknown-linux-gnu.so libcozo_c.so
+
+# Decompress the downloaded gzipped library file
+echo "Decompress downloaded gzipped library file"
+sudo gzip -d ./libcozo_c-0.7.2-x86_64-unknown-linux-gnu.so.gz
+
+# Move the decompressed library file to /usr/lib/x86_64-linux-gnu/ and rename it to libcozo_c.so
+echo "Move decompressed library to /usr/lib/x86_64-linux-gnu/libcozo_c.so"
+sudo mv ./libcozo_c-0.7.2-x86_64-unknown-linux-gnu.so /usr/lib/x86_64-linux-gnu/libcozo_c.so
+
+# Create a symbolic link named libcozo_c.so that points to the library file in /usr/lib/x86_64-linux-gnu/
+echo "Create symbolic link"
+ln -s /usr/lib/x86_64-linux-gnu/libcozo_c.so libcozo_c.so
+ 
+echo "Done!"
